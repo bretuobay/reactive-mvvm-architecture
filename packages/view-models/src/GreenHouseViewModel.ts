cfb084a49a48a37c5ecc8672f2c3d6b10c36f3a2
Festus Yeboah <festus.yeboah@konux.de>
2025-06-11 23:16:18 +0200

    Use createReactiveViewModel to create and example

--- packages/view-models/src/GreenHouseViewModel.ts
+++ packages/view-models/src/GreenHouseViewModel.ts
@@ -1,23 +1,21 @@
-import { RestfulApiViewModel } from "mvvm-core";
-// import { GreenHouseModel } from "@repo/models";
-import { GreenHouseModel } from "../../models/src/GreenHouseModel";
+import { createReactiveViewModel, ViewModelFactoryConfig } from "mvvm-core";
+import { config as modelConfig } from "../../models/src/GreenHouseModel";
 
-import { type GreenhouseListData, GreenhouseListSchema } from "../../models";
+import {
+  type GreenhouseListData,
+  GreenhouseListSchema,
+} from "../../models/src/schemas";
 
-// Refactor  this so that we pass a config object to a function that has dataType and schema
-// and model then the creates the GreenHouseViewModel and returns it as greenHouseViewModel
-
-export class GreenHouseViewModel extends RestfulApiViewModel<
+type TConfig = ViewModelFactoryConfig<
   GreenhouseListData,
   typeof GreenhouseListSchema
-> {
-  constructor(model: GreenHouseModel) {
-    super(model);
-    this.model = model;
-  }
-}
+>;
 
-const greenHouseModel = new GreenHouseModel();
-export const greenHouseViewModel = new GreenHouseViewModel(greenHouseModel);
+const config: TConfig = {
+  modelConfig,
+  schema: GreenhouseListSchema,
+};
+
+export const greenHouseViewModel = createReactiveViewModel(config);
 
 export type { GreenhouseListData };
